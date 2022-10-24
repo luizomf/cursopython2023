66f8b729f33105f80d53df0f96c1a7d990c4cf2f
Luiz Otávio <luizomf@gmail.com>
2022-10-24 11:06:13 -0300

    Comentários e DocStrings

---
+++ aula1.py
@@ -0,0 +1,15 @@
+"""
+DocString
+E escrever o que eu
+quiser
+
+
+asdfasdfd
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
