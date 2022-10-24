ae16ef23dd1e90b13e901f12dbe51adb2b5a6cea
Luiz Otávio <luizomf@gmail.com>
2022-10-24 14:01:08 -0300

    Tipo de dados bool (boolean) - True ou False

---
+++ aula5.py
@@ -0,0 +1,13 @@
+# Tipo de dado bool (boolean)
+# Ao questionar algo em um programa,
+# só existem duas respostas possíveis:
+# sim (True) ou não (False).
+# Existem vários operadores para "questionar".
+# Dentre eles o ==, que é um operador lógico que
+# questiona se um valor é igual a outro
+print(10 == 10)  # Sim => True (Verdadeiro)
+print(10 == 11)  # Não => False (Falso)
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(10 == 11))
