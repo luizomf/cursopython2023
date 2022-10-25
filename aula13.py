49eafd2d7011a643097a3b145327394c1f7cbab1
Luiz Otávio <luizomf@gmail.com>
2022-10-25 10:48:58 -0300

    Uma introdução às f-strings (formatação de strings)

---
+++ aula13.py
@@ -0,0 +1,17 @@
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+# Luiz Otávio tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é
+# 29.320987654320987
