c419716498b9afe4dd7055199ddc461968e4faaf
Luiz Otávio <luizomf@gmail.com>
2022-10-24 23:19:46 -0300

    Exercício com variáveis e tipos de dados (solucionado)

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Luiz Otávio'
+sobrenome = 'Miranda'
+idade = 18
+ano_nascimento = 2022 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.80
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
