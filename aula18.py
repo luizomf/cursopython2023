3b50436ea5cf01b686c8d400ae9d856fbfc4e1fa
Luiz Otávio <luizomf@gmail.com>
2022-10-25 14:06:54 -0300

    O Debugger do VS Code e o interpretador do Python lendo os códigos

---
+++ aula18.py
@@ -0,0 +1,24 @@
+# if / elif      / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
