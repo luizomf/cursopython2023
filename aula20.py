ec29eff794fa7f1817dde8a2d409d361231b34f9
Luiz Otávio <luizomf@gmail.com>
2022-10-25 18:23:11 -0300

    Solução - Exercício de programação com if e comparação

---
+++ aula20.py
@@ -0,0 +1,13 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(
+        f'{primeiro_valor=} é maior ou igual '
+        f'ao que {segundo_valor=}'
+    )
+else:
+    print(
+        f'{segundo_valor=} é maior '
+        f'do que {primeiro_valor=}'
+    )
