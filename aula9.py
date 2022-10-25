44d5903acf9a281b9d2525bb34e5900321349c3d
Luiz Otávio <luizomf@gmail.com>
2022-10-25 09:10:08 -0300

    Introdução aos operadores aritméticos (matemática)

---
+++ aula9.py
@@ -0,0 +1,26 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 3  # float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 3
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
