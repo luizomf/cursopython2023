37f7d63352a5e4cd6e22ad3826f40ae23246bd75
Luiz Otávio <luizomf@gmail.com>
2022-10-25 10:19:18 -0300

    Solução exercício de programação - Cálculo do IMC

---
+++ aula12.py
@@ -0,0 +1,12 @@
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# Luiz Otávio tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é
+# 29.320987654320987
