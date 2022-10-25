35dca53369f1cb2d33d55becd5e960aa0251077c
Luiz Otávio <luizomf@gmail.com>
2022-10-25 18:02:25 -0300

    Operadores relacionais (de comparação) em Python

---
+++ aula19.py
@@ -0,0 +1,17 @@
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print('Olha meu print aqui')
