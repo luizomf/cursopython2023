c56e0f16138f8dacc7db5185a60c66ad403c45d6
Luiz Otávio <luizomf@gmail.com>
2022-10-26 07:24:36 -0300

    Operador Lógico "or"

---
+++ aula22.py
@@ -0,0 +1,24 @@
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# or - Qualquer condição verdadeira avalia
+# toda a expressão como verdadeira.
+# Se qualquer valor for considerado verdadeiro,
+# a expressão inteira será avaliada naquele valor.
+# São considerados falsy (que vc já viu)
+# 0 0.0 '' False
+# Também existe o tipo None que é
+# usado para representar um não valor
+
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '123456'
+
+# if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+
+# Avaliação de curto circuito
+senha = input('Senha: ') or 'Sem senha'
+print(senha)
