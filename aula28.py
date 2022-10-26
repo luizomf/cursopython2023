8295420924d8c2e5cd28ef3f0749182d5ca8868a
Luiz Otávio <luizomf@gmail.com>
2022-10-26 12:14:30 -0300

    Exercício: teste seu conhecimento até aqui

---
+++ aula28.py
@@ -0,0 +1,15 @@
+"""
+Exercício
+Peça ao usuário para digitar seu nome
+Peça ao usuário para digitar sua idade
+Se nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Seu nome contém (ou não) espaços
+        Seu nome tem {n} letras
+        A primeira letra do seu nome é {letra}
+        A última letra do seu nome é {letra}
+Se nada for digitado em nome ou idade: 
+    exiba "Desculpe, você deixou campos vazios."
+"""
