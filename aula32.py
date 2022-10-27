33faded61e80444b18a8210d9403d40592bd9bc3
Luiz Otávio <luizomf@gmail.com>
2022-10-27 11:49:14 -0300

    Solução 2 dos Exercícios - Enunciados

--- aula32.py
+++ aula32.py
@@ -3,7 +3,7 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-entrada = input('Digite um número: ')
+# entrada = input('Digite um número: ')
 
 # if entrada.isdigit():
 #     entrada_int = int(entrada)
@@ -17,23 +17,37 @@ entrada = input('Digite um número: ')
 # else:
 #     print('Você não digitou um número inteiro')
 
-try:
-    entrada_int = float(entrada)
-    par_impar = entrada_int % 2 == 0
-    par_impar_texto = 'ímpar'
+# try:
+#     entrada_int = float(entrada)
+#     par_impar = entrada_int % 2 == 0
+#     par_impar_texto = 'ímpar'
 
-    if par_impar:
-        par_impar_texto = 'par'
+#     if par_impar:
+#         par_impar_texto = 'par'
 
-    print(f'O número {entrada_int} é {par_impar_texto}')
-except:
-    print('Você não digitou um número inteiro')
+#     print(f'O número {entrada_int} é {par_impar_texto}')
+# except:
+#     print('Você não digitou um número inteiro')
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
+entrada = input('Digite a hora em números inteiros: ')
 
+try:
+    hora = int(entrada)
+
+    if hora >= 0 and hora <= 11:
+        print('Bom dia')
+    elif hora >= 12 and hora <= 17:
+        print('Bom tarde')
+    elif hora >= 18 and hora <= 23:
+        print('Bom noite')
+    else:
+        print('Não conheço essa hora')
+except:
+    print('Por favor, digite apenas números inteiros')
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
