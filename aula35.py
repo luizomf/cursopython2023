121fe51cf61ef486d2757898893f1126835b1321
Luiz Otávio <luizomf@gmail.com>
2022-10-27 18:42:06 -0300

    While - Condição em detalhes

---
+++ aula35.py
@@ -0,0 +1,13 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+contador = 0
+
+while contador <= 10:
+    contador = contador + 1
+    print(contador)
+
+print('Acabou')
