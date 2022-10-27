52994846438aaad92d40788260fb12522aa3082a
Luiz Otávio <luizomf@gmail.com>
2022-10-27 11:35:22 -0300

    Solução 1 dos Exercícios - Enunciados

--- aula32.py
+++ aula32.py
@@ -3,7 +3,31 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
+entrada = input('Digite um número: ')
 
+# if entrada.isdigit():
+#     entrada_int = int(entrada)
+#     par_impar = entrada_int % 2 == 0
+#     par_impar_texto = 'ímpar'
+
+#     if par_impar:
+#         par_impar_texto = 'par'
+
+#     print(f'O número {entrada_int} é {par_impar_texto}')
+# else:
+#     print('Você não digitou um número inteiro')
+
+try:
+    entrada_int = float(entrada)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'ímpar'
+
+    if par_impar:
+        par_impar_texto = 'par'
+
+    print(f'O número {entrada_int} é {par_impar_texto}')
+except:
+    print('Você não digitou um número inteiro')
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
