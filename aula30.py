3ef37fda771bb7e9b0c2bd8ae11ed23c537e6547
Luiz Otávio <luizomf@gmail.com>
2022-10-27 09:39:27 -0300

    Parte 2: Variáveis, constantes e complexidade de código

--- aula30.py
+++ aula30.py
@@ -9,3 +9,17 @@ local_carro = 100  # local em que o carro está na estrada
 RADAR_1 = 60  # velocidade máxima do radar 1
 LOCAL_1 = 100  # local onde o radar 1 está
 RADAR_RANGE = 1  # A distância onde o radar pega
+
+vel_carro_pass_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and \
+    local_carro <= (LOCAL_1 + RADAR_RANGE)
+carro_multado_radar_1 = carro_passou_radar_1 and vel_carro_pass_radar_1
+
+if vel_carro_pass_radar_1:
+    print('Velocidade carro passou do radar 1')
+
+if carro_passou_radar_1:
+    print('Carro passou radar 1')
+
+if carro_multado_radar_1:
+    print('carro multado em radar 1')
