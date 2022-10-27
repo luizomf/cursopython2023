9afcf1a97349f5f08d03eb8be417c188128bdc34
Luiz Otávio <luizomf@gmail.com>
2022-10-27 12:53:41 -0300

    Documentação

---
+++ aula33.py
@@ -0,0 +1,9 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = '1000'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(outra_variavel)
+print(string.zfill(10))
