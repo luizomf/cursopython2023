49bbc2ee125b79ed23d54d41c66733de0ceb83c3
Luiz Otávio <luizomf@gmail.com>
2022-10-27 19:34:40 -0300

    while + while

---
+++ aula38.py
@@ -0,0 +1,19 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
+
+
+print('Acabou')
