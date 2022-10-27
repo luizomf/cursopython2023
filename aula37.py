68a07eb8e176028c228516a63c14533e85c8ec2d
Luiz Otávio <luizomf@gmail.com>
2022-10-27 19:17:32 -0300

    while + continue

---
+++ aula37.py
@@ -0,0 +1,26 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+contador = 0
+
+while contador <= 100:
+    contador += 1
+
+    if contador == 6:
+        print('Não vou mostrar o 6.')
+        continue
+
+    if contador >= 10 and contador <= 27:
+        print('Não vou mostrar o', contador)
+        continue
+
+    print(contador)
+
+    if contador == 40:
+        break
+
+
+print('Acabou')
