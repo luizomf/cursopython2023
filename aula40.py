9fbb8ff8837c12618365ad7b3ac4728e1f1c3b38
Luiz Otávio <luizomf@gmail.com>
2022-10-28 07:39:08 -0300

    Exercício guiado - Calculadora - Parte 2

--- aula40.py
+++ aula40.py
@@ -1,7 +1,34 @@
 """ Calculadora com while """
 while True:
-    print('nummmmm')
-    #########
+    numero_1 = input('Digite um número: ')
+    numero_2 = input('Digite outro número: ')
+    operador = input('Digite o operador (+-/*): ')
+
+    numeros_validos = None
+
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados são inválidos.')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    ###
+
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
 
     if sair is True:
