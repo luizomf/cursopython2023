75f3f24a653fc1daa0f3b496daff273177400192
Luiz Otávio <luizomf@gmail.com>
2022-10-28 12:18:13 -0300

    O que aprendemos com while também funciona no for (continue, break, else, etc)

---
+++ aula46.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    if i == 2:
+        print('i é 2, pulando...')
+        continue
+
+    if i == 8:
+        print('i é 8, seu else não executará')
+        break
+
+    for j in range(1, 3):
+        print(i, j)
+else:
+    print('For completo com sucesso!')
