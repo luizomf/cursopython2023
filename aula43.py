8b61cd45457e5048390b5f9209fcc400be955be8
Luiz Otávio <luizomf@gmail.com>
2022-10-28 10:52:13 -0300

    Introdução ao for / in - estrutura de repetição para coisas finitas

---
+++ aula43.py
@@ -0,0 +1,18 @@
+# senha_salva = '123456'
+# senha_digitada = ''
+# repeticoes = 0
+
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Sua senha ({repeticoes}x): ')
+
+#     repeticoes += 1
+
+# print(repeticoes)
+# print('Aquele laço acima pode ter repetições infinitas')
+texto = 'Python'
+
+novo_texto = ''
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto + '*')
