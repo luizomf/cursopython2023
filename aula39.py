444d56b9f3c7f0d7b4db5b3151d455b6f4aa7421
Luiz Otávio <luizomf@gmail.com>
2022-10-28 05:59:11 -0300

    Exercício Guiado com while

--- aula39.py
+++ aula39.py
@@ -1,3 +1,13 @@
 """
-Crie uma calculadora com while
+Iterando strings com while
 """
+#       012345678910
+nome = 'Luiz Otávio'  # Iteráveis
+#      1110987654321
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+print(nome[3])
+
+nova_string = ''
+nova_string += '*L*u*i*z* *O*t*á*v*i*o'
