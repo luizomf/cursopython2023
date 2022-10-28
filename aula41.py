69b7c76071902625f03beaffc10ba3b514b382c6
Luiz Otávio <luizomf@gmail.com>
2022-10-28 08:25:20 -0300

    while / else

---
+++ aula41.py
@@ -0,0 +1,15 @@
+""" while/else """
+string = 'Valor qualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Não encontrei um espaço na string.')
+print('Fora do while.')
