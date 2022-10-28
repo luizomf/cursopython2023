70ce1b9601930f04837fb2c3fa4e8400ad598e9c
Luiz Otávio <luizomf@gmail.com>
2022-10-28 07:19:17 -0300

    Exercício guiado - Calculadora - Parte 1

---
+++ aula40.py
@@ -0,0 +1,8 @@
+""" Calculadora com while """
+while True:
+    print('nummmmm')
+    #########
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+
+    if sair is True:
+        break
