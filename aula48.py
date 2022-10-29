50fb09af9e6fea62308350695d092143e60aa232
Luiz Otávio <luizomf@gmail.com>
2022-10-29 12:29:07 -0300

    Concatenando e estendendo listas

--- aula48.py
+++ aula48.py
@@ -14,12 +14,8 @@ Métodos úteis:
 Create Read Update   Delete
 Criar, ler, alterar, apagar = lista[i] (CRUD)
 """
-#        0   1   2   3
-lista = [10, 20, 30, 40]
-lista.append('Luiz')
-nome = lista.pop()
-lista.append(1233)
-del lista[-1]
-# lista.clear()
-lista.insert(100, 5)
-print(lista[4])
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+lista_c = lista_a + lista_b
+lista_a.extend(lista_b)
+print(lista_a)
