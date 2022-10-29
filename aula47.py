85e3eff2d54b1e24aa96093c46d7d5e77c60075d
Luiz Otávio <luizomf@gmail.com>
2022-10-29 09:18:20 -0300

    Exercício - Jogo da palavra secreta

---
+++ aula47.py
@@ -0,0 +1,16 @@
+"""
+Faça um jogo para o usuário adivinhar qual
+a palavra secreta.
+- Você vai propor uma palavra secreta
+qualquer e vai dar a possibilidade para
+o usuário digitar apenas uma letra.
+- Quando o usuário digitar uma letra, você 
+vai conferir se a letra digitada está
+na palavra secreta.
+    - Se a letra digitada estiver na
+    palavra secreta; exiba a letra;
+    - Se a letra digitada não estiver
+    na palavra secreta; exiba *.
+Faça a contagem de tentativas do seu
+usuário.
+"""
