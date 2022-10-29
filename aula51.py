8562941779365442616438a3a74e478a61a38d5f
Luiz Otávio <luizomf@gmail.com>
2022-10-29 14:00:05 -0300

    Introdução ao empacotamento e desempacotamento

--- aula51.py
+++ aula51.py
@@ -1,6 +1,5 @@
 """
-Introdução ao desempacotamento + tuplas (tuple)
+Introdução ao empacotamento e desempacotamento
 """
-nome1, *_ = 'Maria', 'Helena', 'Luiz'
-a, b, c = 1, 2, 3
-print(a, b, c)
+_, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
+print(nome)
