b62a2aa141c435941a36f67380faeadecba81915
Luiz Otávio <luizomf@gmail.com>
2022-10-29 15:58:47 -0300

    Exercício - crie uma lista de compras com listas

---
+++ aula54.py
@@ -0,0 +1,7 @@
+"""
+Faça uma lista de comprar com listas
+O usuário deve ter a possibilidade de
+inserir, apagar e listar valores da sua lista
+Não permita que o programa quebre com 
+erros de índices inexistentes na lista.
+"""
