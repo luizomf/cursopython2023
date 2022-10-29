952acfec22dedad81c42b1305ab2564d3da3c021
Luiz Otávio <luizomf@gmail.com>
2022-10-29 10:37:24 -0300

    (Parte 1) Solução do exercício - Jogo da palavra secreta

--- aula47.py
+++ aula47.py
@@ -14,3 +14,25 @@ na palavra secreta.
 Faça a contagem de tentativas do seu
 usuário.
 """
+import os
+
+palavra_secreta = 'perfume'
+letras_acertadas = ''
+numero_tentativas = 0
+
+while True:
+    letra_digitada = input('Digite uma letra: ')
+    numero_tentativas += 1
+
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra.')
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            print(letra_secreta)
+        else:
+            print('*')
