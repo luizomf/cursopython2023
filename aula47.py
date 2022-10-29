06714511609940c132648e7ffecb2bb06ec36f0b
Luiz Otávio <luizomf@gmail.com>
2022-10-29 10:39:43 -0300

    (Parte 2) Solução do exercício - Jogo da palavra secreta

--- aula47.py
+++ aula47.py
@@ -31,8 +31,19 @@ while True:
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
 
+    palavra_formada = ''
     for letra_secreta in palavra_secreta:
         if letra_secreta in letras_acertadas:
-            print(letra_secreta)
+            palavra_formada += letra_secreta
         else:
-            print('*')
+            palavra_formada += '*'
+
+    print('Palavra formada:', palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        os.system('clear')
+        print('VOCÊ GANHOU!! PARABÉNS!')
+        print('A palavra era', palavra_secreta)
+        print('Tentativas:', numero_tentativas)
+        letras_acertadas = ''
+        numero_tentativas = 0
