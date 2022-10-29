23c2edb4a4242dceb55000bb6117fb27656aadb3
Luiz Otávio <luizomf@gmail.com>
2022-10-29 12:48:06 -0300

    Cuidados com tipos de dados mutáveis - list e copy

--- aula48.py
+++ aula48.py
@@ -1,21 +1,11 @@
 """
-Listas em Python
-Tipo list - Mutável
-Suporta vários valores de qualquer tipo
-Conhecimentos reutilizáveis - índices e fatiamento
-Métodos úteis:
-    append - Adiciona um item ao final
-    insert - Adiciona um item no índice escolhido
-    pop - Remove do final ou do índice escolhido
-    del - apaga um índice
-    clear - limpa a lista
-    extend - estende a lista
-    + - concatena listas
-Create Read Update   Delete
-Criar, ler, alterar, apagar = lista[i] (CRUD)
+Cuidados com dados mutáveis
+= - copiado o valor (imutáveis)
+= - aponta para o mesmo valor na memória (mutável)
 """
-lista_a = [1, 2, 3]
-lista_b = [4, 5, 6]
-lista_c = lista_a + lista_b
-lista_a.extend(lista_b)
+lista_a = ['Luiz', 'Maria', 1, True, 1.2]
+lista_b = lista_a.copy()
+
+lista_a[0] = 'Qualquer coisa'
 print(lista_a)
+print(lista_b)
