1e34fa7fafe1fe376cdb4fe764523a5d0df5eb54
Luiz Otávio <luizomf@gmail.com>
2022-10-30 12:29:37 -0300

    Listas dentro de listas (iteráveis dentro de iteráveis)

---
+++ aula57.py
@@ -0,0 +1,21 @@
+"""
+Lista de listas e seus índices
+"""
+salas = [
+    # 0        1
+    ['Maria', 'Helena', ],  # 0
+    # 0
+    ['Elaine', ],  # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduarda', ],  # 2
+]
+
+# print(salas[1][0])
+# print(salas[0][1])
+# print(salas[2][2])
+# print(salas[2][3][3])
+
+for sala in salas:
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
