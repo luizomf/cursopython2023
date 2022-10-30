283397cba482b36ab179c68e7c4d25c3c219a106
Luiz Otávio <luizomf@gmail.com>
2022-10-30 17:01:30 -0300

    Operação ternária com Python (if e else de uma linha)

---
+++ aula60.py
@@ -0,0 +1,12 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condicao> else <outro valor>
+"""
+# condicao = 10 == 11
+# variavel = 'Valor' if condicao else 'Outro valor'
+# print(variavel)
+# digito = 9  # > 9 = 0
+# novo_digito = digito if digito <= 9 else 0
+# novo_digito = 0 if digito > 9 else digito
+# print(novo_digito)
+print('Valor' if False else 'Outro valor' if False else 'Fim')
