ff345eb33e2f23723aeebe36ea303147395ed21b
Luiz Otávio <luizomf@gmail.com>
2022-10-30 11:36:33 -0300

    split, join e strip são métodos muito úteis da str

---
+++ aula56.py
@@ -0,0 +1,16 @@
+"""
+split e join com list e str
+split - divide uma string (list)
+join - une uma string
+"""
+frase = '   Olha só que   , coisa interessante          '
+lista_frases_cruas = frase.split(',')
+
+lista_frases = []
+for i, frase in enumerate(lista_frases_cruas):
+    lista_frases.append(lista_frases_cruas[i].strip())
+
+# print(lista_frases_cruas)
+# print(lista_frases)
+frases_unidas = ', '.join(lista_frases)
+print(frases_unidas)
