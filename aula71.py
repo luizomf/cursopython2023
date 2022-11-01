9d9938c31d48f32ea0efaa81297a3382d975fe62
Luiz Otávio <luizomf@gmail.com>
2022-11-01 10:29:45 -0300

    *args para quantidade de argumentos não nomeados variáveis

---
+++ aula71.py
@@ -0,0 +1,31 @@
+"""
+args - Argumentos não nomeados
+* - *args (empacotamento e desempacotamento)
+"""
+# Lembre-te de desempacotamento
+# x, y, *resto = 1, 2, 3, 4
+# print(x, y, resto)
+
+
+# def soma(x, y):
+#     return x + y
+
+def soma(*args):
+    total = 0
+    for numero in args:
+        total += numero
+    return total
+
+
+soma_1_2_3 = soma(1, 2, 3)
+# print(soma_1_2_3)
+
+soma_4_5_6 = soma(4, 5, 6)
+# print(soma_4_5_6)
+
+numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+outra_soma = soma(*numeros)
+print(outra_soma)
+
+print(sum(numeros))
+# print(*numeros)
