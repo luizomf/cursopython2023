dd20f84f726f3d1df42ca26202bd9601b28d4ebf
Luiz Otávio <luizomf@gmail.com>
2022-11-01 12:14:59 -0300

    Higher Order Functions - Funções de primeira classe

---
+++ aula73.py
@@ -0,0 +1,20 @@
+"""
+Higher Order Functions
+Funções de primeira classe
+"""
+
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}!'
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+print(
+    executa(saudacao, 'Bom dia', 'Luiz')
+)
+print(
+    executa(saudacao, 'Boa noite', 'Maria')
+)
