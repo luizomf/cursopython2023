a842aef8c6d5a7b2f33c5446f9e22d8c6c15251b
Luiz Otávio <luizomf@gmail.com>
2022-11-02 08:37:34 -0300

    Introdução ao tipo de dados dict - Dicionários em Python

---
+++ aula76.py
@@ -0,0 +1,23 @@
+# Dicionários em Python (tipo dict)
+# Dicionários são estruturas de dados do tipo
+# par de "chave" e "valor".
+# Chaves podem ser consideradas como o "índice"
+# que vimos na lista e podem ser de tipos imutáveis
+# como: str, int, float, bool, tuple, etc.
+# O valor pode ser de qualquer tipo, incluindo outro
+# dicionário.
+# Usamos as chaves - {} - ou a classe dict para criar
+# dicionários.
+# Imutáveis: str, int, float, bool, tuple
+# Mutável: dict, list
+pessoa = {
+    'nome': 'Luiz Otávio',
+    'sobrenome': 'Miranda',
+    'idade': 18,
+    'altura': 1.8,
+    'endereços': [
+        {'rua': 'tal tal', 'número': 123},
+        {'rua': 'outra rua', 'número': 321},
+    ]
+}
+print(pessoa, type(pessoa))
