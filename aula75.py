06b180c7d355176901923dcfcfdc08e8c7032194
Luiz Otávio <luizomf@gmail.com>
2022-11-02 07:59:26 -0300

    Solução do exercício com funções

--- aula75.py
+++ aula75.py
@@ -1,3 +1,26 @@
 # Exercícios
 # Crie funções que duplicam, triplicam e quadruplicam
 # o número recebido como parâmetro.
+# def duplicar(numero):
+#     return numero * 2
+
+
+# def triplicar(numero):
+#     return numero * 3
+
+
+# def quadruplicar(numero):
+#     return numero * 4
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+
+print(duplicar(2))
+print(triplicar(2))
+print(quadruplicar(2))
