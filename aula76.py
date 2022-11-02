c7f0684de081718988e6a45d62d4ff386fcfc0e7
Luiz Otávio <luizomf@gmail.com>
2022-11-02 08:51:54 -0300

    Introdução ao tipo de dados dict - Dicionários em Python

--- aula76.py
+++ aula76.py
@@ -10,6 +10,17 @@
 # dicionários.
 # Imutáveis: str, int, float, bool, tuple
 # Mutável: dict, list
+# pessoa = {
+#     'nome': 'Luiz Otávio',
+#     'sobrenome': 'Miranda',
+#     'idade': 18,
+#     'altura': 1.8,
+#     'endereços': [
+#         {'rua': 'tal tal', 'número': 123},
+#         {'rua': 'outra rua', 'número': 321},
+#     ]
+# }
+# pessoa = dict(nome='Luiz Otávio', sobrenome='Miranda')
 pessoa = {
     'nome': 'Luiz Otávio',
     'sobrenome': 'Miranda',
@@ -18,6 +29,13 @@ pessoa = {
     'endereços': [
         {'rua': 'tal tal', 'número': 123},
         {'rua': 'outra rua', 'número': 321},
-    ]
+    ],
 }
-print(pessoa, type(pessoa))
+# print(pessoa, type(pessoa))
+print(pessoa['nome'])
+print(pessoa['sobrenome'])
+
+print()
+
+for chave in pessoa:
+    print(chave, pessoa[chave])
