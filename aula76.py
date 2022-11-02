b67e894d775197c17ebf316a47e810d1949be33e
Luiz Otávio <luizomf@gmail.com>
2022-11-02 09:33:32 -0300

    Manipulando chaves e valores em dicionários

--- aula76.py
+++ aula76.py
@@ -1,26 +1,11 @@
-# Dicionários em Python (tipo dict)
-# Dicionários são estruturas de dados do tipo
-# par de "chave" e "valor".
-# Chaves podem ser consideradas como o "índice"
-# que vimos na lista e podem ser de tipos imutáveis
-# como: str, int, float, bool, tuple, etc.
-# O valor pode ser de qualquer tipo, incluindo outro
-# dicionário.
-# Usamos as chaves - {} - ou a classe dict para criar
-# dicionários.
-# Imutáveis: str, int, float, bool, tuple
-# Mutável: dict, list
-# pessoa = {
-#     'nome': 'Luiz Otávio',
-#     'sobrenome': 'Miranda',
-#     'idade': 18,
-#     'altura': 1.8,
-#     'endereços': [
-#         {'rua': 'tal tal', 'número': 123},
-#         {'rua': 'outra rua', 'número': 321},
-#     ]
-# }
-# pessoa = dict(nome='Luiz Otávio', sobrenome='Miranda')
+# Manipulando chaves e valores em dicionários
+from pprint import pprint
+
+
+def p(dicionario):
+    pprint(dicionario, sort_dicts=False)
+
+
 pessoa = {
     'nome': 'Luiz Otávio',
     'sobrenome': 'Miranda',
@@ -31,11 +16,28 @@ pessoa = {
         {'rua': 'outra rua', 'número': 321},
     ],
 }
-# print(pessoa, type(pessoa))
-print(pessoa['nome'])
-print(pessoa['sobrenome'])
+pessoa['nome'] = 'João'
+pessoa[(1, 2, 3)] = 'EITA'
+del pessoa[(1, 2, 3)]
+del pessoa['nome']
+del pessoa['endereços']
+pessoa['teste de atenção'] = 'ATENÇÃO'
+print(pessoa['teste de atenção'])
+pessoa['teste de atenção'] = 'NOVA ATENÇÃO'
+del pessoa['teste de atenção']
+p(pessoa)
 
-print()
+# for chave in pessoa:
+#     if isinstance(chave, tuple):
+#         for valor in chave:
+#             print(valor, '<<-- DA TUPLA')
 
-for chave in pessoa:
-    print(chave, pessoa[chave])
+# chave_dinamica = 'nome'
+# # print(pessoa[chave_dinamica])
+
+# if pessoa.get(chave_dinamica) is None:
+#     print('Chave não existe')
+# else:
+#     print(pessoa.get(chave_dinamica))
+
+# print(pessoa.get(chave_dinamica))
