a7298a6205fb1a2c64466151c97c316c8a42e8fd
Luiz Otávio <luizomf@gmail.com>
2022-11-02 12:27:44 -0300

    Exercício - sistema de perguntas e respostas com Python

---
+++ aula77.py
@@ -0,0 +1,20 @@
+# Exercício - sistema de perguntas e respostas
+
+
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5*5?',
+        'Opções': ['25', '55', '10', '51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opções': ['4', '5', '2', '1'],
+        'Resposta': '5',
+    },
+]
