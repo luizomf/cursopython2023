0e4d9db7025c89b0db61bdd285c64f4c8eeac47a
Luiz Otávio <luizomf@gmail.com>
2022-11-03 07:45:44 -0300

    Métodos úteis do tipo set em Python

--- aula78.py
+++ aula78.py
@@ -28,6 +28,14 @@
 
 # Métodos úteis:
 # add, update, clear, discard
+s1 = set()
+s1.add('Luiz')
+s1.add(1)
+s1.update(('Olá mundo', 1, 2, 3, 4))
+# s1.clear()
+s1.discard('Olá mundo')
+s1.discard('Luiz')
+print(s1)
 
 # Operadores úteis:
 # união | união (union) - Une
