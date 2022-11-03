48ddd7e7877e3edc4f06b708ecf0eabe649b6ad8
Luiz Otávio <luizomf@gmail.com>
2022-11-03 09:13:26 -0300

    Solução Exercício - Encontre o primeiro duplicado considerando a segunda ocorrência

--- aula80.py
+++ aula80.py
@@ -25,3 +25,24 @@ lista_de_listas_de_inteiros = [
     [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
     [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
 ]
+
+
+def encontra_primeiro_duplicado(lista_de_inteiros):
+    numeros_checados = set()
+    primeiro_duplicado = -1
+
+    for numero in lista_de_inteiros:
+        if numero in numeros_checados:
+            primeiro_duplicado = numero
+            break
+
+        numeros_checados.add(numero)
+
+    return primeiro_duplicado
+
+
+for lista in lista_de_listas_de_inteiros:
+    print(
+        lista,
+        encontra_primeiro_duplicado(lista)
+    )
