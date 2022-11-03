76dfe805abb218bc9f23b4a72a46aa33cdd4d377
Luiz Otávio <luizomf@gmail.com>
2022-11-03 10:14:05 -0300

    Funções lambda complexas (para entendimento)

---
+++ aula82.py
@@ -0,0 +1,34 @@
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+# def soma(x, y):
+#     return x + y
+
+
+# def cria_multiplicador(multiplicador):
+#     def multiplica(numero):
+#         return numero * multiplicador
+#     return multiplica
+
+
+# duplica = cria_multiplicador(2)
+duplica = executa(
+    lambda m: lambda n: n * m,
+    2
+)
+print(duplica(2))
+
+print(
+    executa(
+        lambda x, y: x + y,
+        2, 3
+    ),
+)
+
+print(
+    executa(
+        lambda *args: sum(args),
+        1, 2, 3, 4, 5, 6, 7
+    )
+)
