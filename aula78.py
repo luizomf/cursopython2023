734d8d28337c6763302f485019806c04aee1a442
Luiz Otávio <luizomf@gmail.com>
2022-11-03 07:54:56 -0300

    Operadores importantes para o tipo set (conjuntos)

--- aula78.py
+++ aula78.py
@@ -35,10 +35,17 @@ s1.update(('Olá mundo', 1, 2, 3, 4))
 # s1.clear()
 s1.discard('Olá mundo')
 s1.discard('Luiz')
-print(s1)
+# print(s1)
 
 # Operadores úteis:
 # união | união (union) - Une
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+s3 = s1 & s2
+s3 = s2 - s1
+s3 = s1 ^ s2
+print(s3)
