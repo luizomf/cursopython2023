15f512e7fd289a496aa10e7ce6f75db07b1ea5cd
Luiz Otávio <luizomf@gmail.com>
2022-11-03 12:17:17 -0300

    Filtro em list comprehension (filter)

--- aula84.py
+++ aula84.py
@@ -2,6 +2,13 @@
 # List comprehension é uma forma rápida para criar listas
 # a partir de iteráveis.
 # print(list(range(10)))
+import pprint
+
+
+def p(v):
+    pprint.pprint(v, sort_dicts=False, width=40)
+
+
 lista = []
 for numero in range(10):
     lista.append(numero)
@@ -26,5 +33,14 @@ novos_produtos = [
     for produto in produtos
 ]
 
+# # print(novos_produtos)
 # print(novos_produtos)
-print(*novos_produtos, sep='\n')
+# p(novos_produtos)
+# lista = [n for n in range(10) if n < 5]
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+    if (produto['preco'] >= 20 and produto['preco'] * 1.05) > 10
+]
+p(novos_produtos)
