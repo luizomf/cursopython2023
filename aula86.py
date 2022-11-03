1fd6ee17931cf74c597cf432bf1fcce9dee3ff0d
Luiz Otávio <luizomf@gmail.com>
2022-11-03 13:01:38 -0300

    Dictionary Comprehension e Set Comprehension

---
+++ aula86.py
@@ -0,0 +1,27 @@
+# Dictionary Comprehension e Set Comprehension
+produto = {
+    'nome': 'Caneta Azul',
+    'preco': 2.5,
+    'categoria': 'Escritório',
+}
+
+dc = {
+    chave: valor.upper()
+    if isinstance(valor, str) else valor
+    for chave, valor
+    in produto.items()
+    if chave != 'categoria'
+}
+
+lista = [
+    ('a', 'valor a'),
+    ('b', 'valor a'),
+    ('b', 'valor a'),
+]
+dc = {
+    chave: valor
+    for chave, valor in lista
+}
+
+s1 = {2 ** i for i in range(10)}
+print(s1)
