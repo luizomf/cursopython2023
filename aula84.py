27b07efeb54ee873bd87ada8b6bea551854e4429
Luiz Otávio <luizomf@gmail.com>
2022-11-03 11:56:02 -0300

    Mapeamento de dados em list comprehension

--- aula84.py
+++ aula84.py
@@ -11,4 +11,20 @@ lista = [
     numero * 2
     for numero in range(10)
 ]
-print(lista)
+# print(list(range(10)))
+# print(lista)
+
+# Mapeamento de dados em list comprehension
+produtos = [
+    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p2', 'preco': 10, },
+    {'nome': 'p3', 'preco': 30, },
+]
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
+
+# print(novos_produtos)
+print(*novos_produtos, sep='\n')
