8c02a11641ffa4a33858cee55c42364115060d3a
Luiz Otávio <luizomf@gmail.com>
2022-11-03 07:35:21 -0300

    Peculiaridades do tipo mutável set em Python

--- aula78.py
+++ aula78.py
@@ -8,16 +8,23 @@
 # Criando um set
 # set(iterável) ou {1, 2, 3}
 # s1 = set('Luiz')
-s1 = set()  # vazio
-s1 = {'Luiz', 1, 2, 3}  # com dados
+# s1 = set()  # vazio
+# s1 = {'Luiz', 1, 2, 3}  # com dados
 
 # Sets são eficientes para remover valores duplicados
 # de iteráveis.
-# - Não aceitam valores mutáveis;
 # - Seus valores serão sempre únicos;
+# - Não aceitam valores mutáveis;
 # - não tem índexes;
 # - não garantem ordem;
 # - são iteráveis (for, in, not in)
+# l1 = [1, 2, 3, 3, 3, 3, 3, 1]
+# s1 = set(l1)
+# l2 = list(s1)
+# s1 = {1, 2, 3}
+# print(3 not in s1)
+# for numero in s1:
+#     print(numero)
 
 # Métodos úteis:
 # add, update, clear, discard
