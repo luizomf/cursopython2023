db8c58ffcd44f24af9eb96601dcba45b3e0f4304
Luiz Otávio <luizomf@gmail.com>
2022-11-03 08:20:48 -0300

    Exemplo de uso do tipo set

---
+++ aula79.py
@@ -0,0 +1,11 @@
+# Exemplo de uso dos sets
+letras = set()
+while True:
+    letra = input('Digite: ')
+    letras.add(letra.lower())
+
+    if 'l' in letras:
+        print('PARABÉNS')
+        break
+
+    print(letras)
