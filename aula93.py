db2d8aac0ff0f395b7db77f06e7fb1f97ef2b6a2
Luiz Otávio <luizomf@gmail.com>
2022-11-04 11:22:22 -0300

    (Parte1) try e except para tratar exceções

---
+++ aula93.py
@@ -0,0 +1,22 @@
+# (Parte1) try e except para tratar exceções
+# a = 18
+# b = 0
+# c = a / b
+
+try:
+    a = 18
+    b = 0
+    # print(b[0])
+    print('Linha 1'[1000])
+    c = a / b
+    print('Linha 2')
+except ZeroDivisionError:
+    print('Dividiu por zero.')
+except NameError:
+    print('Nome b não está definido')
+except (TypeError, IndexError):
+    print('TypeError + IndexError')
+except Exception:
+    print('ERRO DESCONHECIDO.')
+
+print('CONTINUAR')
