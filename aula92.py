031291cdac86ea4cc0b8edddd47399dae8713646
Luiz Otávio <luizomf@gmail.com>
2022-11-04 10:54:54 -0300

    yield from

---
+++ aula92.py
@@ -0,0 +1,39 @@
+# Yield from
+def gen1():
+    print('COMECOU GEN1')
+    yield 1
+    yield 2
+    yield 3
+    print('ACABOU GEN1')
+
+
+def gen3():
+    print('COMECOU GEN3')
+    yield 10
+    yield 20
+    yield 30
+    print('ACABOU GEN3')
+
+
+def gen2(gen=None):
+    print('COMECOU GEN2')
+    if gen is not None:
+        yield from gen
+    yield 4
+    yield 5
+    yield 6
+    print('ACABOU GEN2')
+
+
+g1 = gen2(gen1())
+g2 = gen2(gen3())
+g3 = gen2()
+for numero in g1:
+    print(numero)
+print()
+for numero in g2:
+    print(numero)
+print()
+for numero in g3:
+    print(numero)
+print()
