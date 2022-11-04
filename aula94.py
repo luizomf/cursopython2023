9dd82e59af011a366b41d5cbd5816b5e60c2df3e
Luiz Otávio <luizomf@gmail.com>
2022-11-04 11:46:53 -0300

    try, except, else e finally + Built-in Exceptions

---
+++ aula94.py
@@ -0,0 +1,17 @@
+# try, except, else e finally
+# https://docs.python.org/pt-br/3/library/exceptions.html#built-in-exceptions
+try:
+    print('ABRIR ARQUIVO')
+    8/0
+except ZeroDivisionError as e:
+    print(e.__class__.__name__)
+    print(e)
+    print('DIVIDIU ZERO')
+except IndexError as error:
+    print('IndexError')
+except (NameError, ImportError):
+    print('NameError, ImportError')
+else:
+    print('Não deu erro')
+finally:
+    print('FECHAR ARQUIVO')
