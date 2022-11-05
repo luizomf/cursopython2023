75af2a9a2cb2bbf067623d96c7e7f5f690c88e6c
Luiz Otávio <luizomf@gmail.com>
2022-11-05 15:40:08 -0300

    Exercício - Adiando execução de funções

--- aula101.py
+++ aula101.py
@@ -7,9 +7,14 @@ def multiplica(x, y):
     return x * y
 
 
-def criar_funcao(funcao, *args):
-    return funcao(*args)
+def criar_funcao(funcao, x):
+    def interna(y):
+        return funcao(x, y)
+    return interna
 
 
 soma_com_cinco = criar_funcao(soma, 5)
 multiplica_por_dez = criar_funcao(multiplica, 10)
+
+print(soma_com_cinco(10))
+print(multiplica_por_dez(10))
