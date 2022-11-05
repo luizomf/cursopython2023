5a88d86dd17e427b6a6e0ecf6922bc73279b83cb
Luiz Otávio <luizomf@gmail.com>
2022-11-05 08:02:14 -0300

    Módulos - import, from, as e *

---
+++ aula96.py
@@ -0,0 +1,42 @@
+# Módulos padrão do Python (import, from, as e *)
+# https://docs.python.org/3/py-modindex.html
+# inteiro - import nome_modulo
+# Vantagens: você tem o namespace do módulo
+# Desvantagens: nomes grandes
+# import sys
+
+# platform = 'A MINHA'
+# print(sys.platform)
+# print(platform)
+
+# partes - from nome_modulo import objeto1, objeto2
+# Vantagens: nomes pequenos
+# Desvantagens: Sem o namespace do módulo
+# from sys import exit, platform
+
+# print(platform)
+
+# alias 1 - import nome_modulo as apelido
+# import sys as s
+
+# sys = 'alguma coisa'
+# print(s.platform)
+# print(sys)
+
+
+# alias 2 - from nome_modulo import objeto as apelido
+# from sys import exit as ex
+# from sys import platform as pf
+
+# print(pf)
+
+# Vantagens: você pode reservar nomes para seu código
+# Desvantagens: pode ficar fora do padrão da linguagem
+
+# má prática - from nome_modulo import *
+# Vantagens: importa tudo de um módulo
+# Desvantagens: importa tudo de um módulo
+# from sys import exit, platform
+
+# print(platform)
+# exit()
