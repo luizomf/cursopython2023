782e3d7f986475be155f1b79392ec2fdf5444951
Luiz Otávio <luizomf@gmail.com>
2022-11-05 09:48:21 -0300

    Recarregando módulos, importlib e singleton

---
+++ aula98.py
@@ -0,0 +1,11 @@
+import importlib
+
+import aula98_m
+
+print(aula98_m.variavel)
+
+for i in range(10):
+    importlib.reload(aula98_m)
+    print(i)
+
+print('Fim')
---
+++ aula98_m.py
@@ -0,0 +1,3 @@
+print(123)
+
+variavel = 'Luiz 1'
