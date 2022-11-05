a05b1a8fe73b8baeab548c1533c4d9ec7910fc65
Luiz Otávio <luizomf@gmail.com>
2022-11-05 20:34:16 -0300

    Variáveis livres + nonlocal (locals, globals)

---
+++ aula102.py
@@ -0,0 +1,33 @@
+# Variáveis livres + nonlocal (locals, globals)
+# print(globals())
+# def fora(x):
+#     a = x
+
+#     def dentro():
+#         # print(locals())
+
+#         return a
+#     return dentro
+
+
+# dentro1 = fora(10)
+# dentro2 = fora(20)
+
+# print(dentro1())
+# print(dentro2())
+def concatenar(string_inicial):
+    valor_final = string_inicial
+
+    def interna(valor_a_concatenar=''):
+        nonlocal valor_final
+        valor_final += valor_a_concatenar
+        return valor_final
+    return interna
+
+
+c = concatenar('a')
+print(c('b'))
+print(c('c'))
+print(c('d'))
+final = c()
+print(final)
