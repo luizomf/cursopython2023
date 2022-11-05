343371a72ef869664060287b11ce59b3388336c1
Luiz Otávio <luizomf@gmail.com>
2022-11-05 14:25:15 -0300

    Proposta de 3 exercícios em um

---
+++ aula100.py
@@ -0,0 +1,17 @@
+# copy, sorted, produtos.sort
+# Exercícios
+# Aumente os preços dos produtos a seguir em 10%
+# Gere novos_produtos por deep copy (cópia profunda)
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+# Ordene os produtos por nome decrescente (do maior para menor)
+# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+
+# Ordene os produtos por preco crescente (do menor para maior)
+# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
