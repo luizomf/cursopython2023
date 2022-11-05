e353b7b2f16ba15c1a2ad2fbf17a9ad743eb8f27
Luiz Otávio <luizomf@gmail.com>
2022-11-05 09:10:17 -0300

    Como importar coisas do seu próprio módulo (ponto de vista do __main__)

--- aula97.py
+++ aula97.py
@@ -9,5 +9,10 @@
 # nos caminhos de sys.path
 
 import aula97_m
+from aula97_m import soma, variavel_modulo
 
-print('Este módulo se chama', __name__)
+# print('Este módulo se chama', __name__)
+print(aula97_m.variavel_modulo)
+print(variavel_modulo)
+print(soma(2, 3))
+print(aula97_m.soma(2, 3))
--- aula97_m.py
+++ aula97_m.py
@@ -1,1 +1,5 @@
-print('Este módulo se chama', __name__)
+variavel_modulo = 'Luiz'
+
+
+def soma(x, y):
+    return x + y
