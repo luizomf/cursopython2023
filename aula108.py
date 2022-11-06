9850d78c9ee56d3f1aa0fad6ac0d50034c2bd5bc
Luiz Otávio <luizomf@gmail.com>
2022-11-06 10:26:30 -0300

    count é um iterador sem fim (itertools)

---
+++ aula108.py
@@ -0,0 +1,21 @@
+# count é um iterador sem fim (itertools)
+from itertools import count
+
+c1 = count(step=8, start=8)
+r1 = range(8, 100, 8)
+
+print('c1', hasattr(c1, '__iter__'))
+print('c1', hasattr(c1, '__next__'))
+print('r1', hasattr(r1, '__iter__'))
+print('r1', hasattr(r1, '__next__'))
+
+print('count')
+for i in c1:
+    if i >= 100:
+        break
+
+    print(i)
+print()
+print('range')
+for i in r1:
+    print(i)
