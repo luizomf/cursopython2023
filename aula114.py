e37cdf25f648039e7f5043fa2356710fa7744b28
Luiz Otávio <luizomf@gmail.com>
2022-11-07 09:57:24 -0300

    Cuidados com funções recursivas

--- aula114.py
+++ aula114.py
@@ -7,18 +7,34 @@
 # - Um caso base que para a recursão
 # - fatorial - n! = 5! = 5 * 4 * 3 * 2 * 1 = 120
 # https://brasilescola.uol.com.br/matematica/fatorial.htm
-def recursiva(inicio=0, fim=4):
+# import sys
 
-    print(inicio, fim)
-
-    # Caso base
-    if inicio >= fim:
-        return fim
-
-    # Caso recursivo
-    # contar até chegar ao final
-    inicio += 1
-    return recursiva(inicio, fim)
+# sys.setrecursionlimit(1004)
 
 
-print(recursiva())
+# def recursiva(inicio=0, fim=4):
+
+#     print(inicio, fim)
+
+#     # Caso base
+#     if inicio >= fim:
+#         return fim
+
+#     # Caso recursivo
+#     # contar até chegar ao final
+#     inicio += 1
+#     return recursiva(inicio, fim)
+
+
+# print(recursiva(0, 1001))
+
+def factorial(n):
+    if n <= 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+print(factorial(5))
+print(factorial(10))
+print(factorial(100))
