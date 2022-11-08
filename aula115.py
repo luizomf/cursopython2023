51e5271a95e6c6e8d8ce2724ae1264a25e46d887
Luiz Otávio <luizomf@gmail.com>
2022-11-08 10:36:46 -0300

    Criando e usando um requirements.txt

--- aula115.py
+++ aula115.py
@@ -29,3 +29,8 @@
 # pip uninstall nome_pacote
 # Congelar (ver pacotes)
 # pip freeze
+#
+# Criando e usando um requirements.txt
+# pip freeze > requirements.txt
+# Instalando tudo do requirements.txt
+# pip install -r requirements.txt
