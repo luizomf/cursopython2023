a5b5dc188e47073d529a86dd8ef7942857dead5c
Luiz Otávio <luizomf@gmail.com>
2022-11-08 10:13:35 -0300

    pip - instalando pacotes e bibliotecas

--- aula115.py
+++ aula115.py
@@ -19,3 +19,13 @@
 # Linux e Mac: source venv/bin/activate
 # Desativar: deactivate
 #
+# pip - instalando pacotes e bibliotecas
+# Instalar última versão:
+# pip install nome_pacote
+# Instalar versão precisa
+# (tem outras formas também não mencionadas)
+# pip install nome_pacote==0.0.0
+# Desinstalar pacote
+# pip uninstall nome_pacote
+# Congelar (ver pacotes)
+# pip freeze
