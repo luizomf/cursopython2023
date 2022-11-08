589c39be171181d67d88d4bee101570ccc40a7e8
Luiz Otávio <luizomf@gmail.com>
2022-11-08 09:45:42 -0300

    Ativando e desativando meu ambiente virtual

--- aula115.py
+++ aula115.py
@@ -13,3 +13,9 @@
 # Abra a pasta do seu projeto no terminal
 # e digite:
 # python -m venv venv
+#
+# Ativando e desativando meu ambiente virtual
+# Windows: .\venv\Scripts\activate
+# Linux e Mac: source venv/bin/activate
+# Desativar: deactivate
+#
