86dff0427df3fbee420a7203a762d39a96e1f62c
Luiz Otávio <luizomf@gmail.com>
2022-11-08 08:06:42 -0300

    O que são ambientes virtuais? (venv)

---
+++ aula115.py
@@ -0,0 +1,10 @@
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalação
+# do Python para uma pasta no caminho escolhido.
+# Ao ativar um ambiente virtual, a instalação do
+# ambiente virtual será usada.
+# venv é o módulo que vamos usar para
+# criar ambientes virtuais.
+# Você pode dar o nome que preferir para um
+# ambiente virtual, mas os mais comuns são:
+# venv env .venv .env
