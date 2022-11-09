f705ee4c9dd43bd52d7dea39c51cb2dc8ffe1d22
Luiz Otávio <luizomf@gmail.com>
2022-11-09 07:38:42 -0300

    Modos de abertura de arquivo e encoding com with open

--- aula116.py
+++ aula116.py
@@ -1,3 +1,4 @@
+# Leia também: https://www.otaviomiranda.com.br/2020/normalizacao-unicode-em-python/
 # with open (context manager) e métodos úteis do TextIOWrapper
 # Usamos a função open para abrir
 # um arquivo em Python (ele pode ou não existir)
@@ -24,27 +25,35 @@ caminho_arquivo = 'aula116.txt'
 # #
 # arquivo.close()
 
-with open(caminho_arquivo, 'w+') as arquivo:
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+#         ('Linha 3\n', 'Linha 4\n')
+#     )
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+#     print('Lendo')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline().strip())
+
+#     print('READLINES')
+#     arquivo.seek(0, 0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
+
+
+# print('#' * 10)
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+
+with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+    arquivo.write('Atenção\n')
     arquivo.write('Linha 1\n')
     arquivo.write('Linha 2\n')
     arquivo.writelines(
         ('Linha 3\n', 'Linha 4\n')
     )
-    arquivo.seek(0, 0)
-    print(arquivo.read())
-    print('Lendo')
-    arquivo.seek(0, 0)
-    print(arquivo.readline(), end='')
-    print(arquivo.readline().strip())
-    print(arquivo.readline().strip())
-
-    print('READLINES')
-    arquivo.seek(0, 0)
-    for linha in arquivo.readlines():
-        print(linha.strip())
-
-
-print('#' * 10)
-
-with open(caminho_arquivo, 'r') as arquivo:
-    print(arquivo.read())
