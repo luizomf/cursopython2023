6a352ad20e8210859bd3f38d78f42be14571faf1
Luiz Otávio <luizomf@gmail.com>
2022-11-10 08:45:12 -0300

    Salvando dados Python em JSON com json

---
+++ aula117.json
@@ -0,0 +1,24 @@
+{
+  "nome": "Luiz Otávio 2",
+  "sobrenome": "Miranda",
+  "enderecos": [
+    {
+      "rua": "R1",
+      "numero": 32
+    },
+    {
+      "rua": "R2",
+      "numero": 55
+    }
+  ],
+  "altura": 1.8,
+  "numeros_preferidos": [
+    2,
+    4,
+    6,
+    8,
+    10
+  ],
+  "dev": true,
+  "nada": null
+}
---
+++ aula117.py
@@ -0,0 +1,28 @@
+import json
+
+pessoa = {
+    'nome': 'Luiz Otávio 2',
+    'sobrenome': 'Miranda',
+    'enderecos': [
+        {'rua': 'R1', 'numero': 32},
+        {'rua': 'R2', 'numero': 55},
+    ],
+    'altura': 1.8,
+    'numeros_preferidos': (2, 4, 6, 8, 10),
+    'dev': True,
+    'nada': None,
+}
+
+with open('aula117.json', 'w', encoding='utf8') as arquivo:
+    json.dump(
+        pessoa,
+        arquivo,
+        ensure_ascii=False,
+        indent=2,
+    )
+
+with open('aula117.json', 'r', encoding='utf8') as arquivo:
+    pessoa = json.load(arquivo)
+    # print(pessoa)
+    # print(type(pessoa))
+    print(pessoa['nome'])
