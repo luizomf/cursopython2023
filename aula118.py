51f35abc862ab54a0218d797506a0a86874ac693
Luiz Otávio <luizomf@gmail.com>
2022-11-10 13:11:55 -0300

    Problema dos parâmetros mutáveis em funções Python

---
+++ aula118.py
@@ -0,0 +1,22 @@
+# Problema dos parâmetros mutáveis em funções Python
+def adiciona_clientes(nome, lista=None):
+    if lista is None:
+        lista = []
+    lista.append(nome)
+    return lista
+
+
+cliente1 = adiciona_clientes('luiz')
+adiciona_clientes('Joana', cliente1)
+adiciona_clientes('Fernando', cliente1)
+cliente1.append('Edu')
+
+cliente2 = adiciona_clientes('Helena')
+adiciona_clientes('Maria', cliente2)
+
+cliente3 = adiciona_clientes('Moreira')
+adiciona_clientes('Vivi', cliente3)
+
+print(cliente1)
+print(cliente2)
+print(cliente3)
