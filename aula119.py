92341be01040f9da946be9ec7c7745f73aa6789d
Luiz Otávio <luizomf@gmail.com>
2022-11-10 20:27:00 -0300

    Exercício - Lista de tarefas com opções de desfazer e refazer

---
+++ aula119.py
@@ -0,0 +1,10 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+# Música para codar =)
+# Everybody wants to rule the world - Tears for fears
+# todo = [] -> lista de tarefas
+# todo = ['fazer café'] -> Adicionar fazer café
+# todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
+# desfazer = ['fazer café',] -> Refazer ['caminhar']
+# desfazer = [] -> Refazer ['caminhar', 'fazer café']
+# refazer = todo ['fazer café']
+# refazer = todo ['fazer café', 'caminhar']
