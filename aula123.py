8a64528b609ee563fb3f5e4f052ef0fc38baa464
Luiz Otávio <luizomf@gmail.com>
2022-11-13 09:50:32 -0300

    Escopo da classe e de métodos da classe

---
+++ aula123.py
@@ -0,0 +1,20 @@
+# Escopo da classe e de métodos da classe
+class Animal:
+    # nome = 'Leão'
+
+    def __init__(self, nome):
+        self.nome = nome
+
+        variavel = 'valor'
+        print(variavel)
+
+    def comendo(self, alimento):
+        return f'{self.nome} está comando {alimento}'
+
+    def executar(self, *args, **kwargs):
+        return self.comendo(*args, **kwargs)
+
+
+leao = Animal(nome='Leão')
+print(leao.nome)
+print(leao.executar('maçã'))
