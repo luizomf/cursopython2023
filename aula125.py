fe425942fbd5e372cee6b1fbd0aceeac5be162b9
Luiz Otávio <luizomf@gmail.com>
2022-11-13 10:32:58 -0300

    Atributos de classe

---
+++ aula125.py
@@ -0,0 +1,20 @@
+# Atributos de classe
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+
+
+p1 = Pessoa('João', 35)
+p2 = Pessoa('Helena', 12)
+
+print(Pessoa.ano_atual)
+# Pessoa.ano_atual = 1
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
