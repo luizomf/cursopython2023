d2b27792b4ca294c42b90a8f4da0f812e9c9b74b
Luiz Otávio <luizomf@gmail.com>
2022-11-13 08:58:29 -0300

    Métodos em instâncias de classes Python

---
+++ aula0121.py
@@ -0,0 +1,20 @@
+# Métodos em instâncias de classes Python
+# Hard coded - É algo que foi escrito diretamente no código
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+
+
+string = 'Luiz'
+print(string.upper())
+
+fusca = Carro('Fusca')
+print(fusca.nome)
+fusca.acelerar()
+
+celta = Carro(nome='Celta')
+print(celta.nome)
+celta.acelerar()
