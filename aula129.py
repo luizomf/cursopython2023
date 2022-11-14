49d770774095c87e96e20b9f4c3c13755c83716e
Luiz Otávio <luizomf@gmail.com>
2022-11-14 11:38:28 -0300

    @staticmethod (métodos estáticos) são inúteis em Python =)

---
+++ aula129.py
@@ -0,0 +1,20 @@
+# @staticmethod (métodos estáticos) são inúteis em Python =)
+# Métodos estáticos são métodos que estão dentro da
+# classe, mas não tem acesso ao self nem ao cls.
+# Em resumo, são funções que existem dentro da sua
+# classe.
+class Classe:
+    @staticmethod
+    def funcao_que_esta_na_classe(*args, **kwargs):
+        print('Oi', args, kwargs)
+
+
+def funcao(*args, **kwargs):
+    print('Oi', args, kwargs)
+
+
+c1 = Classe()
+c1.funcao_que_esta_na_classe(1, 2, 3)
+funcao(1, 2, 3)
+Classe.funcao_que_esta_na_classe(nomeado=1)
+funcao(nomeado=1)
