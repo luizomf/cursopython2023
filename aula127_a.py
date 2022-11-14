92078fa670460aaa45cbd61e326e04264b3dc2cf
Luiz Otávio <luizomf@gmail.com>
2022-11-14 09:31:03 -0300

    Exercício - Salve sua classe em JSON

---
+++ aula127_a.py
@@ -0,0 +1,5 @@
+# Exercício - Salve sua classe em JSON
+# Salve os dados da sua classe em JSON
+# e depois crie novamente as instâncias
+# da classe com os dados salvos
+# Faça em arquivos separados.
