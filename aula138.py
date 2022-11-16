4dbc9a3357d8b1f4a7bf84d26444842e5ffe21c8
Luiz Otávio <luizomf@gmail.com>
2022-11-16 08:36:28 -0300

    Herança simples - Relações entre classes

--- aula138.py
+++ aula138.py
@@ -8,3 +8,32 @@
 #   -> super class, base class, parent class
 # Classes filhas (Cliente)
 #   -> sub class, child class, derived class
+class Pessoa:
+    cpf = '1234'
+
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+    def falar_nome_classe(self):
+        print('Classe PESSOA')
+        print(self.nome, self.sobrenome, self.__class__.__name__)
+
+
+class Cliente(Pessoa):
+    def falar_nome_classe(self):
+        print('EITA, nem saí da classe CLIENTE')
+        print(self.nome, self.sobrenome, self.__class__.__name__)
+
+
+class Aluno(Pessoa):
+    cpf = 'cpf aluno'
+    ...
+
+
+c1 = Cliente('Luiz', 'Otávio')
+c1.falar_nome_classe()
+a1 = Aluno('Maria', 'Helena')
+a1.falar_nome_classe()
+print(a1.cpf)
+# help(Cliente)
