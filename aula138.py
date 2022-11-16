f8b99e618413712d10bb684fdaea8649fd186d5a
Luiz Otávio <luizomf@gmail.com>
2022-11-16 08:04:51 -0300

    TEORIA: Herança, generalização e especialização

---
+++ aula138.py
@@ -0,0 +1,10 @@
+# Herança simples - Relações entre classes
+# Associação - usa, Agregação - tem
+# Composição - É dono de, Herança - É um
+#
+# Herança vs Composição
+#
+# Classe principal (Pessoa)
+#   -> super class, base class, parent class
+# Classes filhas (Cliente)
+#   -> sub class, child class, derived class
