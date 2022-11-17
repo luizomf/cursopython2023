1a3f04fcaa367bda80248d77a6d680aabdb3fa5e
Luiz Otávio <luizomf@gmail.com>
2022-11-17 09:32:24 -0300

    Herança Múltipla - Python Orientado a Objetos

--- aula140.py
+++ aula140.py
@@ -26,3 +26,35 @@
 # Para saber a ordem de chamada dos métodos
 # Use o método de classe Classe.mro()
 # Ou o atributo __mro__ (Dunder - Double Underscore)
+class A:
+    ...
+
+    def quem_sou(self):
+        print('A')
+
+
+class B(A):
+    ...
+
+    # def quem_sou(self):
+    #     print('B')
+
+
+class C(A):
+    ...
+
+    def quem_sou(self):
+        print('C')
+
+
+class D(B, C):
+    ...
+
+    def quem_sou(self):
+        print('D')
+
+
+d = D()
+d.quem_sou()
+# print(D.__mro__)
+print(D.mro())
