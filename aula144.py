7fb319a301cc8757f9bbc44216ac79a607898040
Luiz Otávio <luizomf@gmail.com>
2022-11-18 11:04:19 -0300

    Teoria: Polimorfismo e Assinatura de Métodos

---
+++ aula144.py
@@ -0,0 +1,13 @@
+# Polimorfismo em Python Orientado a Objetos
+# Polimorfismo é o princípio que permite que
+# classes deridavas de uma mesma superclasse
+# tenham métodos iguais (com mesma assinatura)
+# mas comportamentos diferentes.
+# Assinatura do método = Mesmo nome e quantidade
+# de parâmetros (retorno não faz parte da assinatura)
+# Opinião + princípios que contam:
+# Assinatura do método: nome, parâmetros e retorno iguais
+# SO"L"ID
+# Princípio da substituição de liskov
+# Objetos de uma superclasse devem ser substituíveis
+# por objetos de uma subclasse sem quebrar a aplicação.
