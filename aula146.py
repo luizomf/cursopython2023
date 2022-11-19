201f49b0f7bf18a27cd2e28b33237e84aba90693
Luiz Otávio <luizomf@gmail.com>
2022-11-19 10:28:58 -0300

    Criando Exceptions em Python Orientado a Objetos

---
+++ aula146.py
@@ -0,0 +1,11 @@
+# Criando Exceptions em Python Orientado a Objetos
+# Para criar uma Exception em Python, você só
+# precisa herdar de alguma exceção da linguagem.
+# A recomendação da doc é herdar de Exception.
+# https://docs.python.org/3/library/exceptions.html
+# Criando exceções (comum colocar Error ao final)
+# Levantando (raise) / Lançando (throw) exceções
+# Relançando exceções
+# Adicionando notas em exceções (3.11.0)
+class MeuError(Exception):
+    ...
