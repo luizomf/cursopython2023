8b1bb23a1eb6936cbcbb1ededd1bc76382b46a0d
Luiz Otávio <luizomf@gmail.com>
2022-11-19 10:31:20 -0300

    Levantando e tratando suas Exceptions (Exceções)

--- aula146.py
+++ aula146.py
@@ -1,11 +1,26 @@
-# Criando Exceptions em Python Orientado a Objetos
-# Para criar uma Exception em Python, você só
-# precisa herdar de alguma exceção da linguagem.
-# A recomendação da doc é herdar de Exception.
+# Levantando e tratando suas Exceptions (Exceções)
 # https://docs.python.org/3/library/exceptions.html
-# Criando exceções (comum colocar Error ao final)
 # Levantando (raise) / Lançando (throw) exceções
 # Relançando exceções
 # Adicionando notas em exceções (3.11.0)
 class MeuError(Exception):
     ...
+
+
+class OutroError(Exception):
+    ...
+
+
+def levantar():
+    exception_ = MeuError('a', 'b', 'c')
+    raise exception_
+
+
+try:
+    levantar()
+except (MeuError, ZeroDivisionError) as error:
+    print(error.__class__.__name__)
+    print(error.args)
+    print()
+    exception_ = OutroError('Vou lançar de novo')
+    raise exception_ from error
