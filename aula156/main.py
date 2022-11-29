50bb704f2c928e8869ae224a83237889b6db3914
Luiz Otávio <luizomf@gmail.com>
2022-11-29 11:06:36 -0300

    DocStrings de várias linhas (Documentação)

--- aula156/main.py
+++ aula156/main.py
@@ -1,7 +1,8 @@
-import uma_linha
+# import uma_linha
+import varias_linha
 
 # print(dir(uma_linha))
 # print(uma_linha.__doc__)
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
-help(uma_linha)
+help(varias_linha)
---
+++ aula156/varias_linha.py
@@ -0,0 +1,18 @@
+"""O que seu módulo faz
+
+Lorem ipsum dolor sit amet. Et praesentium nisi non quam mollitia At saepe 
+quisquam qui quae voluptatem. Eum laudantium impedit qui velit quia ea 
+galisum nihil. Sed consequatur culpa qui corrupti officia eos odit tenetur 
+cum corrupti beatae At provident error cum eveniet consectetur qui perferendis 
+placeat. Non quibusdam blanditiis est rerum repellat aut facere rerum in 
+nihil reiciendis ut maxime galisum qui error accusamus.
+
+Ut cumque enim ut possimus ullam non magni doloribus! 
+Ut quasi doloremque aut itaque molestiae eos fugiat 
+deleniti sed voluptates nisi At eveniet quia et quasi 
+vero ea fugiat delectus! In beatae perferendis in modi 
+possimus eum quaerat maiores ab autem natus ab ullam 
+itaque hic nobis autem ad harum tempore. Qui dolore eius 
+et quasi aliquid et praesentium fuga sed deserunt adipisci eos 
+dolor nemo qui animi dolore et odio libero.
+"""
