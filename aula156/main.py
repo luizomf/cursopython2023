5b19efe784675bbfb4145bf2df4772de345677b1
Luiz Otávio <luizomf@gmail.com>
2022-11-29 11:11:11 -0300

    Fix typo: varias_linha.py to varias_linhas.py

--- aula156/main.py
+++ aula156/main.py
@@ -1,8 +1,8 @@
 # import uma_linha
-import varias_linha
+import varias_linhas
 
 # print(dir(uma_linha))
 # print(uma_linha.__doc__)
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
-help(varias_linha)
+help(varias_linhas)
