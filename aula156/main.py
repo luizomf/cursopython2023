6316384787a364e59781ed4a4f04ac1534832189
Luiz Otávio <luizomf@gmail.com>
2022-11-29 10:47:01 -0300

    dir e help + DocStrings de uma linha (Documentação)

---
+++ aula156/main.py
@@ -0,0 +1,7 @@
+import uma_linha
+
+# print(dir(uma_linha))
+# print(uma_linha.__doc__)
+# print(uma_linha.__file__)
+# print(uma_linha.__name__)
+help(uma_linha)
---
+++ aula156/uma_linha.py
@@ -0,0 +1,7 @@
+"""O que seu módulo faz"""
+
+variavel = 'valor'
+
+
+def funcao():
+    return 1
