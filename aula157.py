9aaf8e6f50145d386faaabdb04f5fa5327a6c4ff
Luiz Otávio <luizomf@gmail.com>
2022-11-30 10:29:32 -0300

    Código: enum.Enum (Enumerações)

--- aula157.py
+++ aula157.py
@@ -14,12 +14,30 @@
 # membro = Classe(valor), Classe['chave']
 # chave = Classe.chave.name
 # valor = Classe.chave.value
+import enum
 
-def mover(direcao):
-    print(f'Movendo para {direcao}')
+# Direcoes = enum.Enum('Direcoes', ['ESQUERDA', 'DIREITA'])
 
 
-mover('esquerda')
-mover('direita')
-mover('acima')
-mover('abaixo')
+class Direcoes(enum.Enum):
+    ESQUERDA = enum.auto()
+    DIREITA = enum.auto()
+    ACIMA = enum.auto()
+    ABAIXO = enum.auto()
+
+
+print(Direcoes(1), Direcoes['ESQUERDA'], Direcoes.ESQUERDA)
+print(Direcoes(1).name, Direcoes.ESQUERDA.value)
+
+
+def mover(direcao: Direcoes):
+    if not isinstance(direcao, Direcoes):
+        raise ValueError('Direção não encontrada')
+
+    print(f'Movendo para {direcao.name} ({direcao.value})')
+
+
+mover(Direcoes.ESQUERDA)
+mover(Direcoes.DIREITA)
+mover(Direcoes.ACIMA)
+mover(Direcoes.ABAIXO)
