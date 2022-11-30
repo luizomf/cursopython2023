c58aed382fde92f4466eda09cc571dc122b80a85
Luiz Otávio <luizomf@gmail.com>
2022-11-30 09:30:53 -0300

    Teoria: enum.Enum (Enumerações)

---
+++ aula157.py
@@ -0,0 +1,25 @@
+# Enum -> Enumerações
+# Enumerações na programação, são usadas em ocasiões onde temos
+# um determinado número de coisas para escolher.
+# Enums têm membros e seus valores são constantes.
+# Enums em python:
+#   - são um conjunto de nomes simbólicos (membros) ligados a valores únicos
+#   - podem ser iterados para retornar seus membros canônicos na ordem de
+#       definição
+# enum.Enum é a superclasse para suas enumerações. Mas também pode ser usada
+#   diretamente (mesmo assim, Enums não são classes normais em Python).
+# Você poderá usar seu Enum com type annotations, com isinstance e
+# outras coisas relacionadas com tipo.
+# Para obter os dados:
+# membro = Classe(valor), Classe['chave']
+# chave = Classe.chave.name
+# valor = Classe.chave.value
+
+def mover(direcao):
+    print(f'Movendo para {direcao}')
+
+
+mover('esquerda')
+mover('direita')
+mover('acima')
+mover('abaixo')
