6a83dca1cd0bb4be769e5146f24205da2e825cfa
Luiz Otávio <luizomf@gmail.com>
2022-12-01 09:19:02 -0300

    Solução - Criando a classe ContaPoupanca

--- aula158/contas.py
+++ aula158/contas.py
@@ -16,3 +16,25 @@ class Conta(abc.ABC):
 
     def detalhes(self, msg=''):
         print(f'O seu saldo é {self.saldo:.2f} {msg}')
+        print('--')
+
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+
+        if valor_pos_saque >= 0:
+            self.saldo -= valor
+            self.detalhes(f'(SAQUE {valor})')
+            return self.saldo
+
+        print('Não foi possível sacar o valor desejado')
+        self.detalhes(f'(SAQUE NEGADO {valor})')
+
+
+if __name__ == '__main__':
+    cp1 = ContaPoupanca(111, 222, 0)
+    cp1.sacar(1)
+    cp1.depositar(1)
+    cp1.sacar(1)
+    cp1.sacar(1)
