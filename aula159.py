14bbe6bef123492259d2b92194ab5a57ee61fdcf
Luiz Otávio <luizomf@gmail.com>
2022-12-03 08:44:30 -0300

    asdict e astuple em dataclasses

--- aula159.py
+++ aula159.py
@@ -5,16 +5,18 @@
 # Em resumo: dataclasses são syntax sugar para criar classes normais.
 # Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
 # doc: https://docs.python.org/3/library/dataclasses.html
-from dataclasses import dataclass
+from dataclasses import asdict, astuple, dataclass
 
 
-@dataclass(repr=True)
+@dataclass
 class Pessoa:
     nome: str
     sobrenome: str
 
 
 if __name__ == '__main__':
-    lista = [Pessoa('A', 'Z'), Pessoa('B', 'Y'), Pessoa('C', 'X')]
-    ordenadas = sorted(lista, reverse=True, key=lambda p: p.sobrenome)
-    print(ordenadas)
+    p1 = Pessoa('Luiz', 'Otávio')
+    print(asdict(p1).keys())
+    print(asdict(p1).values())
+    print(asdict(p1).items())
+    print(astuple(p1)[0])
