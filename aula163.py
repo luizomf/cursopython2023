ebf3c76863aa856a621488372e69e7248b79270b
Luiz Otávio <luizomf@gmail.com>
2022-12-12 11:38:30 -0300

    datetime.timedelta e dateutil.relativetimedelta (calculando datas)

---
+++ aula163.py
@@ -0,0 +1,24 @@
+# datetime.timedelta e dateutil.relativetimedelta (calculando datas)
+# Docs:
+# https://dateutil.readthedocs.io/en/stable/relativedelta.html
+# https://docs.python.org/3/library/datetime.html#timedelta-objects
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+
+fmt = '%d/%m/%Y %H:%M:%S'
+data_inicio = datetime.strptime('20/04/1987 09:30:30', fmt)
+data_fim = datetime.strptime('12/12/2022 08:20:20', fmt)
+# delta = timedelta(days=10, hours=2)
+delta = relativedelta(data_fim, data_inicio)
+print(delta.days, delta.years)
+# print(data_fim - delta)
+# print(data_fim)
+# print(data_fim + relativedelta(seconds=60, minutes=10))
+
+# delta = data_fim - data_inicio
+# print(delta.days, delta.seconds, delta.microseconds)
+# print(delta.total_seconds())
+# print(data_fim > data_inicio)
+# print(data_fim < data_inicio)
+# print(data_fim == data_inicio)
