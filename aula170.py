56eac0f8accaec82762c4cf387afd1a3bdb188c8
Luiz Otávio <luizomf@gmail.com>
2022-12-16 06:53:09 -0300

    os.listdir para navegar em caminhos

---
+++ aula170.py
@@ -0,0 +1,17 @@
+# os.listdir para navegar em caminhos
+# /Users/luizotavio/Desktop/EXEMPLO
+# C:\Users\luizotavio\Desktop\EXEMPLO
+# caminho = r'C:\\Users\\luizotavio\\Desktop\\EXEMPLO'
+import os
+
+caminho = os.path.join('/Users', 'luizotavio', 'Desktop', 'EXEMPLO')
+
+for pasta in os.listdir(caminho):
+    caminho_completo_pasta = os.path.join(caminho, pasta)
+    print(pasta)
+
+    if not os.path.isdir(caminho_completo_pasta):
+        continue
+
+    for imagem in os.listdir(caminho_completo_pasta):
+        print('  ', imagem)
