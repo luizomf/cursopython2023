2a298da04921f2d71ca612a53a66ed8df4b4066c
Luiz Otávio <luizomf@gmail.com>
2022-12-16 07:55:54 -0300

    os.walk para navegar de caminhos de forma recursiva

---
+++ aula171.py
@@ -0,0 +1,23 @@
+# os.walk para navegar de caminhos de forma recursiva
+# os.walk é uma função que permite percorrer uma estrutura de diretórios de
+# maneira recursiva. Ela gera uma sequência de tuplas, onde cada tupla possui
+# três elementos: o diretório atual (root), uma lista de subdiretórios (dirs)
+# e uma lista dos arquivos do diretório atual (files).
+import os
+from itertools import count
+
+caminho = os.path.join('/Users', 'luizotavio', 'Desktop', 'EXEMPLO')
+counter = count()
+
+for root, dirs, files in os.walk(caminho):
+    the_counter = next(counter)
+    print(the_counter, 'Pasta atual', root)
+
+    for dir_ in dirs:
+        print('  ', the_counter, 'Dir:', dir_)
+
+    for file_ in files:
+        caminho_completo_arquivo = os.path.join(root, file_)
+        print('  ', the_counter, 'FILE:', caminho_completo_arquivo)
+        # NÃO FAÇA ISSO (VAI APAGAR TUDO DA PASTA)
+        # os.unlink(caminho_completo_arquivo)
