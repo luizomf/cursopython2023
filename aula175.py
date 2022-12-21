5bcd4388e133da9a087577b0d493c4c276e32356
Luiz Otávio <luizomf@gmail.com>
2022-12-21 08:49:18 -0300

    O que é JSON - JavaScript Object Notation

---
+++ aula175.py
@@ -0,0 +1,24 @@
+# O que é JSON - JavaScript Object Notation
+# JSON - JavaScript Object Notation (extensão .json)
+# É uma estrutura de dados que permite a serialização
+# de objetos em texto simples para facilitar a transmissão de
+# dados através da rede, APIs web ou outros meios de comunicação.
+# O JSON suporta os seguintes tipos de dados:
+# Números: podem ser inteiros ou com ponto flutuante, como 42 ou 3.14
+# Strings: são cadeias de caracteres, como "Olá, mundo!" ou "12345"
+#   As strings devem ser envolvidas por aspas duplas
+# Booleanos: são os valores verdadeiro (true) ou falso (false)
+# Arrays: são listas ordenadas de valores, como [1, 2, 3] ou
+#   ["Oi", "Olá", "Bom dia"]
+# Objetos: são conjuntos de pares chave/valor -> {"nome": "João", "idade": 30}
+# null: é um valor especial que representa ausência de valor
+#
+# Ao converter de Python para JSON:
+# Python        JSON
+# dict          object
+# list, tuple   array
+# str           string
+# int, float    number
+# True          true
+# False         false
+# None          null
