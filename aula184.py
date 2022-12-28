12c91d26809e6b19fed9d52585050d646e523e50
Luiz Otávio <luizomf@gmail.com>
2022-12-28 18:29:01 -0300

    Variáveis de ambiente com Python

---
+++ aula184.py
@@ -0,0 +1,22 @@
+# Variáveis de ambiente com Python
+# Para variáveis de ambiente
+# Windows PS: $env:VARIAVEL="VALOR" | dir env:
+# Linux e Mac: export NOME_VARIAVEL="VALOR" | echo $VARIAVEL
+# Para obter o valor das variáveis de ambiente
+# os.getenv ou os.environ['VARIAVEL']
+# Para configurar variáveis de ambiente
+# os.environ['VARIAVEL'] = 'valor'
+# Ou usando python-dotenv e o arquivo .env
+# pip install python-dotenv
+# from dotenv import load_dotenv
+# load_dotenv()
+# https://pypi.org/project/python-dotenv/
+# OBS.: sempre lembre-se de criar um .env-example
+import os
+
+from dotenv import load_dotenv  # type: ignore
+
+load_dotenv()
+
+# print(os.environ)
+print(os.getenv('BD_PASSWORD'))
