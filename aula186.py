863c23f42f60009c0de827d007bff120980cf3cb
Luiz Otávio <luizomf@gmail.com>
2023-01-02 09:20:24 -0300

    ZIP - Compactando / Descompactando arquivos com zipfile.ZipFile

--- aula186.py
+++ aula186.py
@@ -29,3 +29,19 @@ def criar_arquivos(qtd: int, zip_dir: Path):
 
 
 criar_arquivos(10, CAMINHO_ZIP_DIR)
+
+# Criando um zip e adicionando arquivos
+with ZipFile(CAMINHO_COMPACTADO, 'w') as zip:
+    for root, dirs, files in os.walk(CAMINHO_ZIP_DIR):
+        for file in files:
+            # print(file)
+            zip.write(os.path.join(root, file), file)
+
+# Lendo arquivos de um zip
+with ZipFile(CAMINHO_COMPACTADO, 'r') as zip:
+    for arquivo in zip.namelist():
+        print(arquivo)
+
+# Extraindo arquivos de um zip
+with ZipFile(CAMINHO_COMPACTADO, 'r') as zip:
+    zip.extractall(CAMINHO_DESCOMPACTADO)
