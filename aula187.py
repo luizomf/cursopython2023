c2bba7cf71498a3f8df6de95fd481cffbd259611
Luiz Otávio <luizomf@gmail.com>
2023-01-04 06:42:35 -0300

    sys.argv - Executando arquivos com argumentos no sistema

---
+++ aula187.py
@@ -0,0 +1,16 @@
+# sys.argv - Executando arquivos com argumentos no sistema
+# Fonte = Fira Code
+import sys
+
+argumentos = sys.argv
+qtd_argumentos = len(argumentos)
+
+if qtd_argumentos <= 1:
+    print('Você não passou argumentos')
+else:
+    try:
+        print(f'Você passou os argumentos {argumentos[1:]}')
+        print(f'Faça alguma coisa com {argumentos[1]}')
+        print(f'Faça outra coisa com {argumentos[2]}')
+    except IndexError:
+        print('Faltam argumentos')
