136274d6b614047f901236a79bd84571e329c18a
Luiz Otávio <luizomf@gmail.com>
2023-02-07 09:08:33 -0300

    Selenium - Selecionando elementos com By, expected_conditions e WebDriverWait

--- aula193/main.py
+++ aula193/main.py
@@ -1,12 +1,18 @@
+# type: ignore
 # Selenium - Automatizando tarefas no navegador
 from pathlib import Path
 from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 # Chrome Options
 # https://peter.sh/experiments/chromium-command-line-switches/
+# Doc Selenium
+# https://selenium-python.readthedocs.io/locating-elements.html
 
 
 # Caminho para a raiz do projeto
@@ -21,7 +27,7 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
     # chrome_options.add_argument('--headless')
     if options is not None:
         for option in options:
-            chrome_options.add_argument(option)  # type: ignore
+            chrome_options.add_argument(option)
 
     chrome_service = Service(
         executable_path=str(CHROME_DRIVER_PATH),
@@ -36,6 +42,8 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
 
 
 if __name__ == '__main__':
+    TIME_TO_WAIT = 10
+
     # Example
     # options = '--headless', '--disable-gpu',
     options = ()
@@ -44,5 +52,13 @@ if __name__ == '__main__':
     # Como antes
     browser.get('https://www.google.com')
 
+    # Espere para encontrar o input
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+        EC.presence_of_element_located(
+            (By.NAME, 'q')
+        )
+    )
+    search_input.send_keys('Hello World!')
+
     # Dorme por 10 segundos
-    sleep(10)
+    sleep(TIME_TO_WAIT)
