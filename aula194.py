2c0dbec2bd46b6bd6c86372596c00718a6d1da65
Luiz Otávio <luizomf@gmail.com>
2023-02-12 07:38:37 -0300

    subprocess para executando programas e comandos externos

--- aula194.py
+++ aula194.py
@@ -20,3 +20,29 @@
 # Comando de exemplo:
 # Windows: ping 127.0.0.1
 # Linux/Mac: ping 127.0.0.1 -c 4
+import subprocess
+import sys
+
+# sys.platform = linux, linux2, darwin, win32
+
+cmd = ['ls -lah /']
+encoding = 'utf_8'
+system = sys.platform
+
+if system == "win32":
+    cmd = ['ping', '127.0.0.1']
+    encoding = 'cp850'
+
+
+proc = subprocess.run(
+    cmd, capture_output=True,
+    text=True, encoding=encoding,
+    shell=True,
+)
+
+print()
+
+# print(proc.args)
+# print(proc.stderr)
+print(proc.stdout)
+# print(proc.returncode)
