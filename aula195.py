ba83ca13cf532332256bdad2aaf035c00e28f253
Luiz Otávio <luizomf@gmail.com>
2023-02-14 15:28:40 -0300

    Jupyter Notebook - Instalação e teste

---
+++ aula195.py
@@ -0,0 +1,10 @@
+# Jupyter Notebook - Instalação e teste
+
+# Essa linha cria uma variável a
+a = 2
+
+# Essa linha tem o valor da variável `a` em dobro
+b = a * 2  # 4
+
+# Essa linha mostra o valor de `a` + `b`
+print(a + b)  # 6
