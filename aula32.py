91068f8c752842ecdc1d3d9158064b0f76a3ff6c
Kleber Forte <kleberfforte@gmail.com>
2023-02-15 22:56:15 -0300

    Update aula32.py

--- aula32.py
+++ aula32.py
@@ -41,9 +41,9 @@ Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 #     if hora >= 0 and hora <= 11:
 #         print('Bom dia')
 #     elif hora >= 12 and hora <= 17:
-#         print('Bom tarde')
+#         print('Boa tarde')
 #     elif hora >= 18 and hora <= 23:
-#         print('Bom noite')
+#         print('Boa noite')
 #     else:
 #         print('Não conheço essa hora')
 # except:
