1c8bb203744020e9614dfb9ce0a65786076dc332
Luiz Otávio <luizomf@gmail.com>
2023-02-18 09:58:12 -0300

    PyPDF2 para manipular arquivos PDF (PdfWriter)

--- aula197/main.py
+++ aula197/main.py
@@ -1,4 +1,4 @@
-# PyPDF2 para manipular arquivos PDF (PdfReader)
+# PyPDF2 para manipular arquivos PDF (PdfWriter)
 # PyPDF2 é uma biblioteca de manipulação de arquivos PDF feita em Python puro,
 # gratuita e de código aberto. Ela é capaz de ler, manipular, escrever e unir
 # dados de arquivos PDF, assim como adicionar anotações, transformar páginas,
@@ -9,7 +9,7 @@
 # pip install pypdf2
 from pathlib import Path
 
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfReader, PdfWriter
 
 PASTA_RAIZ = Path(__file__).parent
 PASTA_ORIGINAIS = PASTA_RAIZ / 'pdfs_originais'
@@ -32,3 +32,10 @@ imagem0 = page0.images[0]
 # print(page0.extract_text())
 # with open(PASTA_NOVA / imagem0.name, 'wb') as fp:
 #     fp.write(imagem0.data)
+
+
+for i, page in enumerate(reader.pages):
+    writer = PdfWriter()
+    with open(PASTA_NOVA / f'page{i}.pdf', 'wb') as arquivo:
+        writer.add_page(page)
+        writer.write(arquivo)  # type: ignore
