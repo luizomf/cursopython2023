41a778c57fb1517087da1c4bbc78134458fb45e0
Luiz Otávio <luizomf@gmail.com>
2023-02-25 10:15:05 -0300

    Instalando o PySide6 no seu ambiente virtual

--- aula201/aula0001.py
+++ aula201/aula0001.py
@@ -24,3 +24,10 @@
 #   Licenças são tópicos complexos, portanto, se oriente sobre elas
 #   antes de usar qualquer software de terceiros.
 #   https://tldrlegal.com/license/gnu-lesser-general-public-license-v3-(lgpl-3)
+import PySide6.QtCore
+
+# Prints PySide6 version
+print(PySide6.__version__)  # type: ignore
+
+# Prints the Qt version used to compile PySide6
+print(PySide6.QtCore.__version__)  # type: ignore
