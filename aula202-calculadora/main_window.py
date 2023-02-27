8c09f2b570c1238e770a6f3e2ebb1ba8a7417d8b
Luiz Otávio <luizomf@gmail.com>
2023-02-27 14:13:18 -0300

    modifica nomes de snake_case para camelCase

--- aula202-calculadora/main_window.py
+++ aula202-calculadora/main_window.py
@@ -7,8 +7,8 @@ class MainWindow(QMainWindow):
 
         # Configurando o layout básico
         self.cw = QWidget()
-        self.v_layout = QVBoxLayout()
-        self.cw.setLayout(self.v_layout)
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
         self.setCentralWidget(self.cw)
 
         # Título da janela
@@ -20,4 +20,4 @@ class MainWindow(QMainWindow):
         self.setFixedSize(self.width(), self.height())
 
     def addWidgetToVLayout(self, widget: QWidget):
-        self.v_layout.addWidget(widget)
+        self.vLayout.addWidget(widget)
