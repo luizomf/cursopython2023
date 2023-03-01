e7684cc5ad4fdbac7562399130f8af2c24cdef1a
Luiz Otávio <luizomf@gmail.com>
2023-03-01 07:44:54 -0300

    Calculadora: configurando o PyQt Dark Theme (qdarktheme) no PySide6

--- aula202-calculadora/main.py
+++ aula202-calculadora/main.py
@@ -5,11 +5,13 @@ from info import Info
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
+from styles import setupTheme
 from variables import WINDOW_ICON_PATH
 
 if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
+    setupTheme()
     window = MainWindow()
 
     # Define o ícone
---
+++ aula202-calculadora/styles.py
@@ -0,0 +1,38 @@
+# QSS - Estilos do QT for Python
+# https://doc.qt.io/qtforpython/tutorials/basictutorial/widgetstyling.html
+# Dark Theme
+# https://pyqtdarktheme.readthedocs.io/en/latest/how_to_use.html
+import qdarktheme
+from variables import (DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR,
+                       PRIMARY_COLOR)
+
+qss = f"""
+    PushButton[cssClass="specialButton"] {{
+        color: #fff;
+        background: {PRIMARY_COLOR};
+    }}
+    PushButton[cssClass="specialButton"]:hover {{
+        color: #fff;
+        background: {DARKER_PRIMARY_COLOR};
+    }}
+    PushButton[cssClass="specialButton"]:pressed {{
+        color: #fff;
+        background: {DARKEST_PRIMARY_COLOR};
+    }}
+"""
+
+
+def setupTheme():
+    qdarktheme.setup_theme(
+        theme='dark',
+        corner_shape='rounded',
+        custom_colors={
+            "[dark]": {
+                "primary": f"{PRIMARY_COLOR}",
+            },
+            "[light]": {
+                "primary": f"{PRIMARY_COLOR}",
+            },
+        },
+        additional_qss=qss
+    )
