da398d04a916c6ee440d077b9c21f7f1deb9fcb8
Luiz Otávio <luizomf@gmail.com>
2023-03-03 08:30:15 -0300

    Calculadora: configurando o "backspace" do display no botão "◀" (D)

--- aula202-calculadora/buttons.py
+++ aula202-calculadora/buttons.py
@@ -30,7 +30,7 @@ class ButtonsGrid(QGridLayout):
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
-            ['C', '◀', '^', '/'],
+            ['C', 'D', '^', '/'],
             ['7', '8', '9', '*'],
             ['4', '5', '6', '-'],
             ['1', '2', '3', '+'],
@@ -77,6 +77,9 @@ class ButtonsGrid(QGridLayout):
 
         if text == 'C':
             self._connectButtonClicked(button, self._clear)
+
+        if text in 'D':
+            self._connectButtonClicked(button, self.display.backspace)
 
         if text in '+-/*^':
             self._connectButtonClicked(
