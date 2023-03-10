a04c0304e7e005eabf329a02a09ae8e24008e3a2
Luiz Otávio <luizomf@gmail.com>
2023-03-10 09:39:45 -0300

    Usando eventFilter e installEventFilter com UI do Qt Designer

--- aula203-qtdesigner/src/main_window.py
+++ aula203-qtdesigner/src/main_window.py
@@ -1,5 +1,8 @@
 import sys
+from typing import cast
 
+from PySide6.QtCore import QEvent, QObject
+from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QApplication, QMainWindow
 from window import Ui_MainWindow
 
@@ -11,9 +14,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.buttonSend.clicked.connect(self.changeLabelResult)  # type: ignore
 
+        self.lineName.installEventFilter(self)
+
     def changeLabelResult(self):
         text = self.lineName.text()
         self.labelResult.setText(text)
+
+    def eventFilter(self, watched: QObject, event: QEvent) -> bool:
+        if event.type() == QEvent.Type.KeyPress:
+            # Tenho certeza que o tipo é KeyPress
+            event = cast(QKeyEvent, event)
+            text = self.lineName.text()
+            self.labelResult.setText(text + event.text())
+
+        return super().eventFilter(watched, event)
 
 
 if __name__ == '__main__':
