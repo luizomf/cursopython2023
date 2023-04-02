d0034ee19d66ef7373e2ecb77f3c1b7da8d2e63c
Luiz Otávio <luizomf@gmail.com>
2023-04-02 09:25:46 -0300

    UPDATE no SQLite com Python

--- aula205/main.py
+++ aula205/main.py
@@ -72,6 +72,13 @@ if __name__ == '__main__':
     connection.commit()
 
     cursor.execute(
+        f'UPDATE {TABLE_NAME} '
+        'SET name="QUALQUER", weight=67.89 '
+        'WHERE id = 2'
+    )
+    connection.commit()
+
+    cursor.execute(
         f'SELECT * FROM {TABLE_NAME}'
     )
 
