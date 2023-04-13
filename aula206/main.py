0e6fbcfb80ebc96ed8656896b6641498aa9ef272
Luiz Otávio <luizomf@gmail.com>
2023-04-13 07:38:38 -0300

    PyMySQL - um cliente MySQL feito em Python Puro

---
+++ aula206/main.py
@@ -0,0 +1,4 @@
+# PyMySQL - um cliente MySQL feito em Python Puro
+# Doc: https://pymysql.readthedocs.io/en/latest/
+# Pypy: https://pypi.org/project/pymysql/
+# GitHub: https://github.com/PyMySQL/PyMySQL
