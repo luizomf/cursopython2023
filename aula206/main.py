396a377b454c51e4698af17bac30321e2a72e55c
Luiz Otávio <luizomf@gmail.com>
2023-04-15 10:05:49 -0300

    Usando python-dotenv e .env com pymysql.connect

--- aula206/main.py
+++ aula206/main.py
@@ -2,13 +2,18 @@
 # Doc: https://pymysql.readthedocs.io/en/latest/
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
+import os
+
+import dotenv
 import pymysql
 
+dotenv.load_dotenv()
+
 connection = pymysql.connect(
-    host='localhost',
-    user='usuario',
-    password='senha',
-    database='base_de_dados',
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE'],
 )
 
 with connection:
