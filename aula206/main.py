87b081fd09db5e03d4e65383cbaf1e44c931b85e
Luiz Otávio <luizomf@gmail.com>
2023-04-15 12:28:18 -0300

    Inserindo valores usando dicionários ao invés de iteráveis

--- aula206/main.py
+++ aula206/main.py
@@ -44,6 +44,23 @@ with connection:
         )
         data = ('Luiz', 18)
         result = cursor.execute(sql, data)  # type: ignore
-        print(sql, data)
+        # print(sql, data)
+        # print(result)
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data2 = {
+            "age": 37,
+            "name": "Le",
+        }
+        result = cursor.execute(sql, data2)  # type: ignore
+        print(sql)
+        print(data2)
         print(result)
     connection.commit()
