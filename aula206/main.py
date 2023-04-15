8b555fb64079455c0c7a05905e650295dee3ebad
Luiz Otávio <luizomf@gmail.com>
2023-04-15 12:45:54 -0300

    Inserindo vários valores com uma consulta só usando iteráveis ou dicionários

--- aula206/main.py
+++ aula206/main.py
@@ -60,7 +60,42 @@ with connection:
             "name": "Le",
         }
         result = cursor.execute(sql, data2)  # type: ignore
+        # print(sql)
+        # print(data2)
+        # print(result)
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data3 = (
+            {"name": "Sah", "age": 33, },
+            {"name": "Júlia", "age": 74, },
+            {"name": "Rose", "age": 53, },
+        )
+        result = cursor.executemany(sql, data3)  # type: ignore
+        # print(sql)
+        # print(data3)
+        # print(result)
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%s, %s) '
+        )
+        data4 = (
+            ("Siri", 22, ),
+            ("Helena", 15, ),
+        )
+        result = cursor.executemany(sql, data4)  # type: ignore
         print(sql)
-        print(data2)
+        print(data4)
         print(result)
     connection.commit()
