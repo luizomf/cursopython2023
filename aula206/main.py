8c342e6875e29816da9443ccfd7f07a9d7837cc2
Luiz Otávio <luizomf@gmail.com>
2023-04-15 09:54:00 -0300

    Conectando no seu servidor de base de dados MySQL com PyMySQL

--- aula206/main.py
+++ aula206/main.py
@@ -2,3 +2,16 @@
 # Doc: https://pymysql.readthedocs.io/en/latest/
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
+import pymysql
+
+connection = pymysql.connect(
+    host='localhost',
+    user='usuario',
+    password='senha',
+    database='base_de_dados',
+)
+
+with connection:
+    with connection.cursor() as cursor:
+        # SQL
+        print(cursor)
