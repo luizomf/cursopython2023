d4b81d488ab823ec90aaa950469a9137b4fcf6f7
Luiz Otávio <luizomf@gmail.com>
2023-04-16 12:36:29 -0300

    Primeira URL e function based view + HttpRequest e HttpResponse + Status Codes

--- aula207_ola_django/project/urls.py
+++ aula207_ola_django/project/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+# HTTP Request <-> HTTP Response
+# MVT (MVC)
+
+
+def my_view(request):
+    print('posso fazer outras coisas')
+    return HttpResponse('Uma mensagem para alguém especial')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', my_view),
 ]
