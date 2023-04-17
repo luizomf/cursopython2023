476988f86184a8368d584d3e00dbd948c47fd64e
Luiz Otávio <luizomf@gmail.com>
2023-04-17 08:09:01 -0300

    Configurando templates globais com DIRS +  extends para herança de templates

--- aula207_ola_django/project/settings.py
+++ aula207_ola_django/project/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
