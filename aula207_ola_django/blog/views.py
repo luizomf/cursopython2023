34374a59c7977dfade2f0ff75b3202096efff9ed
Luiz Otávio <luizomf@gmail.com>
2023-04-21 11:11:57 -0300

    Usando o context para enviar dados para dentro dos templates

--- aula207_ola_django/blog/views.py
+++ aula207_ola_django/blog/views.py
@@ -4,15 +4,28 @@ from django.shortcuts import render
 
 def blog(request):
     print('blog')
+
+    context = {
+        'text': 'Olá blog'
+    }
+
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context
     )
 
 
 def exemplo(request):
     print('exemplo')
+
+    context = {
+        'text': 'Olá exemplo',
+        'title': 'Essa é uma página de exemplo - ',
+    }
+
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context
     )
--- aula207_ola_django/home/views.py
+++ aula207_ola_django/home/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render
 def home(request):
     print('home')
 
+    context = {
+        'text': 'Olá home'
+    }
+
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context,
     )
