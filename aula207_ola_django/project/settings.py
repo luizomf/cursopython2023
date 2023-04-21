dc90353228259c4d1d2d022ad8b49bc7d0d05309
Luiz Otávio <luizomf@gmail.com>
2023-04-21 09:14:54 -0300

    Arquivos estáticos (staticfiles), STATIC_URL, STATICFILES_DIRS e load static

--- aula207_ola_django/project/settings.py
+++ aula207_ola_django/project/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base' / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
