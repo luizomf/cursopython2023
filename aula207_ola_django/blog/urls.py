5cd6cc2c0fb9c8b05f6e8f088c0700c11736b794
Luiz Otávio <luizomf@gmail.com>
2023-04-24 08:52:39 -0300

    Trabalhando com URLs dinâmicas em urls.py e nos templates HTML do Django

--- aula207_ola_django/blog/urls.py
+++ aula207_ola_django/blog/urls.py
@@ -1,8 +1,10 @@
 from blog import views
 from django.urls import path
 
+app_name = 'blog'
+
 # blog/
 urlpatterns = [
-    path('', views.blog),
-    path('exemplo/', views.exemplo),
+    path('', views.blog, name='home'),
+    path('exemplo/', views.exemplo, name='exemplo'),
 ]
--- aula207_ola_django/home/urls.py
+++ aula207_ola_django/home/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'home'
+
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
 ]
