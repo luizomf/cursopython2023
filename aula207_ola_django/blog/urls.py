5f1f4e36aefa31bec3bbd03148d3ef816888a200
Luiz Otávio <luizomf@gmail.com>
2023-04-27 10:48:27 -0300

    Configurando um post único no template post.html

--- aula207_ola_django/blog/urls.py
+++ aula207_ola_django/blog/urls.py
@@ -8,6 +8,6 @@ app_name = 'blog'
 # https://docs.djangoproject.com/en/4.2/topics/http/urls/
 urlpatterns = [
     path('', views.blog, name='home'),
-    path('<int:id>/', views.post, name='post'),
+    path('<int:post_id>/', views.post, name='post'),
     path('exemplo/', views.exemplo, name='exemplo'),
 ]
--- aula207_ola_django/blog/views.py
+++ aula207_ola_django/blog/views.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 from blog.data import posts
+from django.http import HttpRequest
 from django.shortcuts import render
 
 
@@ -17,17 +20,26 @@ def blog(request):
     )
 
 
-def post(request, id):
-    print('post', id)
+def post(request: HttpRequest, post_id: int):
+    found_post: dict[str, Any] | None = None
+
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+
+    if found_post is None:
+        raise Exception('Post não existe.')
 
     context = {
         # 'text': 'Olá blog',
-        'posts': posts
+        'post': found_post,
+        'title': found_post['title'] + ' - ',
     }
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post.html',
         context
     )
 
