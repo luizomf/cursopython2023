8a9fdecb25ef06775d25cd8b6a387a7241aab4e8
Luiz Otávio <luizomf@gmail.com>
2023-04-27 13:51:25 -0300

    Sobre os arquivos estáticos, DEBUG e ALLOWED_HOSTS

--- aula207_ola_django/project/settings.py
+++ aula207_ola_django/project/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base' / 'static'
 ]
+# https://whitenoise.readthedocs.io/en/latest/
+STATIC_ROOT = BASE_DIR / 'static_files'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
